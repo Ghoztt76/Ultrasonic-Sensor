0436d31d697a0be16fa1d55d135f43e4c41e17b9
Ghoztt76 <danielyim626@gmail.com>
2024-11-26 11:12:41 -0600

    Update sensor_rough_draft.py

--- sensor_rough_draft.py
+++ sensor_rough_draft.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import random
 SPEED_OF_SOUND = 343
 
 TRIG = 26
@@ -7,7 +8,7 @@ ECHO = 27
 BUTTON = 24  # can change later*
 
 GREEN = 17   #  ^^^
-YELLOW1 = 16
+YELLOW1 = 16 # 1 is closer to sensor
 YELLOW2 = 13
 RED1 = 5 
 RED2 = 6
@@ -52,27 +53,109 @@ def getDistance():
 
     return distance
 
-#def checkLights(dist):
+
+# if getDistance == +- 3 cm of user distance, light turns on 
+
+
+def Lights(dist):
+    active = True
+    target = dist
+
     
+    while(active):
+        actual = getDistance()
+        '''
+        if(actual-target <= '''lower range'''  or actual-target>= '''upper range''')
+            GPIO.output(RED1, GPIO.HIGH)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(GREEN, GPIO.LOW)
+        elif(actual-target<=)'''
+        
+        if(actual-target <= (target+1)  or actual-target>= (target-1)) # GREEN
+            GPIO.output(GREEN, GPIO.HIGH)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(RED1, GPIO.LOW)
+            break
+            
+        elif(actual-target < (target-1)  or actual-target>= (target-9)) # YELLOW1
+            GPIO.output(RED1, GPIO.HIGH)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(GREEN, GPIO.LOW)
+        elif(actual-target <= (target-9)) # Red1
+            GPIO.output(RED1, GPIO.HIGH)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(GREEN, GPIO.LOW)
+        elif(actual-target <= '''lower range'''  or actual-target>= '''upper range''')# Yellow2
+            GPIO.output(RED1, GPIO.HIGH)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(GREEN, GPIO.LOW)
+                if(actual-target <= '''lower range'''  or actual-target>= '''upper range''') # RED2
+            GPIO.output(RED1, GPIO.HIGH)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(GREEN, GPIO.LOW) 
+        
+        '''
+        if(getDistance() <= ((dist/5))):
+            GPIO.output(RED1, GPIO.HIGH)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(GREEN, GPIO.LOW)
+        elif(getDistance() < ((dist/5)*2)):
+            GPIO.output(YELLOW1, GPIO.HIGH)
+            GPIO.output(RED1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(GREEN, GPIO.LOW)
+        elif(getDistance() == dist): 
+            GPIO.output(GREEN, GPIO.HIGH)
+            GPIO.output(RED1, GPIO.LOW)
+            GPIO.output(RED2, GPIO.LOW)
+            GPIO.output(YELLOW2, GPIO.LOW)
+            GPIO.output(YELLOW1, GPIO.LOW)
+            
+        elif(getDistance() < ((dist/5)*3))
+              #yellow2
+        elif(getDistance() < ((dist/5)*4))
+            #red2
+    #1
+'''
 
 
 
 
 
 
-
-
-
-
-
-
+#10sec
 
 def main():
     active = True
     while(active == True):
+        user_dist = int(input("Enter desired dist in cm (1-30): "))
         
         
-    
+        
+        
+        
+        
+        
+    i = raw_input("--Get another measurement (Y/n)? ")
+    # stop measuring if desired
+    if (not i in [ "y", "Y", "yes", "Yes", "YES", "" ]):
+        break
+        active = False
 
 
 main()
